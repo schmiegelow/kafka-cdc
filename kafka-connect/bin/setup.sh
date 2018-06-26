eeef8b0c50ffae392042713efff20cf7a5f156cc
Erik Schmiegelow <erik.schmiegelow@gmail.com>
2018-06-26 20:10:23 +0200

    Added jdbc connect

---
+++ kafka-connect/bin/setup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+curl -X POST -H "Content-Type: application/json" \
+http://localhost:8083/connectors \
+  -d '{
+ "name": "elasticsearch-sink",
+  "config": {
+    "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
+    "tasks.max": "1",
+    "topics": "analyzed",
+    "key.ignore": "true",
+    "topic.schema.ignore":"true",
+    "schema.ignore":"true",
+    "connection.url": "http://elasticsearch:9200",
+    "type.name": "kafka-connect",
+    "name": "elasticsearch-sink"
+  }
+}'
+
+curl -X POST \
+  -H "Content-Type: application/json" \
+  --data '{ "name": "quickstart-jdbc-source",
+  "config": {
+  "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
+  "tasks.max": 1,
+  "connection.url":
+  "jdbc:mysql://127.0.0.1:3306/connect_test?user=root&password=confluent",
+  "mode": "incrementing",
+  "incrementing.column.name": "id",
+  "timestamp.column.name": "modified",
+  "topic.prefix": "quickstart-jdbc-",
+  "poll.interval.ms": 1000 } }' \
+  http://localhost:8083/connectors
